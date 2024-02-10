e29a9b342d194c26ace2d4b121f5bf91bbce0190
Gallus Huber <e51905700@student.tuwien.ac.at>
2024-02-10 15:49:24 +0100

    add simple language-server

---
+++ extension/src/extension.ts
@@ -0,0 +1,81 @@
+
+import * as vscode from 'vscode';
+import * as path from 'path';
+import { registerDefaultCommands } from 'sprotty-vscode';
+import { LspWebviewPanelManager } from 'sprotty-vscode/lib/lsp';
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
+
+let languageClient: LanguageClient;
+
+export function activate(context: vscode.ExtensionContext) {
+    //Create output channel
+    let orange = vscode.window.createOutputChannel("Orange");
+
+    //Write to output.
+    orange.appendLine("I am a banana.");
+
+    const openHelp = 'Open Help';
+    vscode.window.showInformationMessage('ER2CDS Extension is active.', ...[openHelp])
+        .then((selection) => {
+            if (selection === openHelp) {
+                vscode.env.openExternal(vscode.Uri.parse('https://github.com/GallusHuber/er2cds'));
+            }
+        });
+
+    languageClient = createLanguageClient(context);
+
+    const webviewPanelManager = new LspWebviewPanelManager({
+        extensionUri: context.extensionUri,
+        defaultDiagramType: 'er2cds',
+        languageClient,
+        supportedFileExtensions: ['.er2cds']
+    });
+
+    registerDefaultCommands(webviewPanelManager, context, { extensionPrefix: 'er2cds' });
+}
+
+function createLanguageClient(context: vscode.ExtensionContext): LanguageClient {
+    const serverModule = context.asAbsolutePath(path.join('..', 'language-server', 'out', 'server.js'));
+
+    // The debug options for the server
+    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging.
+    // By setting `process.env.DEBUG_BREAK` to a truthy value, the language server will wait until a debugger is attached.
+    const debugOptions = { execArgv: ['--nolazy', `--inspect${process.env.DEBUG_BREAK ? '-brk' : ''}=${process.env.DEBUG_SOCKET || '6009'}`] };
+
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    const serverOptions: ServerOptions = {
+        run: { module: serverModule, transport: TransportKind.ipc },
+        debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+    };
+
+    const fileSystemWatcher = vscode.workspace.createFileSystemWatcher('**/*.er2cds');
+    context.subscriptions.push(fileSystemWatcher);
+
+    // Options to control the language client
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+        synchronize: {
+            // Notify the server about file changes to files contained in the workspace
+            fileEvents: fileSystemWatcher
+        }
+    };
+
+    // Create the language client and start the client.
+    const languageClient = new LanguageClient(
+        'er2cds',
+        'er2cds',
+        serverOptions,
+        clientOptions
+    );
+
+    // Start the client. This will also launch the server
+    languageClient.start();
+    return languageClient;
+}
+
+export async function deactivate(): Promise<void> {
+    if (languageClient) {
+        await languageClient.stop();
+    }
+}
---
+++ language-server/src/server.ts
@@ -0,0 +1,145 @@
+import {
+    createConnection,
+    TextDocuments,
+    Diagnostic,
+    DiagnosticSeverity,
+    ProposedFeatures,
+    InitializeParams,
+    DidChangeConfigurationNotification,
+    CompletionItem,
+    CompletionItemKind,
+    TextDocumentPositionParams,
+    TextDocumentSyncKind,
+    InitializeResult
+} from 'vscode-languageserver/node';
+
+import { TextDocument } from 'vscode-languageserver-textdocument';
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+let connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability: boolean = false;
+let hasWorkspaceFolderCapability: boolean = false;
+let hasDiagnosticRelatedInformationCapability: boolean = false;
+
+connection.onInitialize((params: InitializeParams) => {
+    let capabilities = params.capabilities;
+
+    // Does the client support the `workspace/configuration` request?
+    // If not, we fall back using global settings.
+    hasConfigurationCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.configuration
+    );
+    hasWorkspaceFolderCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.workspaceFolders
+    );
+    hasDiagnosticRelatedInformationCapability = !!(
+        capabilities.textDocument &&
+        capabilities.textDocument.publishDiagnostics &&
+        capabilities.textDocument.publishDiagnostics.relatedInformation
+    );
+
+    const result: InitializeResult = {
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Incremental,
+            // Tell the client that this server supports code completion.
+            completionProvider: {
+                resolveProvider: true
+            }
+        }
+    };
+
+    if (hasWorkspaceFolderCapability) {
+        result.capabilities.workspace = {
+            workspaceFolders: {
+                supported: true
+            }
+        };
+    }
+
+    return result;
+});
+
+connection.onInitialized(() => {
+    if (hasConfigurationCapability) {
+        // Register for all configuration changes.
+        connection.client.register(DidChangeConfigurationNotification.type, undefined);
+    }
+    if (hasWorkspaceFolderCapability) {
+        connection.workspace.onDidChangeWorkspaceFolders(_event => {
+            connection.console.log('Workspace folder change event received.');
+        });
+    }
+});
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent(change => {
+    let diagnostics: Diagnostic[] = [];
+
+    let diagnostic: Diagnostic = {
+        severity: DiagnosticSeverity.Warning,
+        range: {
+            start: change.document.positionAt(0),
+            end: change.document.positionAt(1)
+        },
+        message: 'Test',
+        source: 'ex'
+    };
+
+    diagnostics.push(diagnostic);
+
+    connection.sendDiagnostics({ uri: change.document.uri, diagnostics });
+});
+
+connection.onDidChangeWatchedFiles(_change => {
+    // Monitored files have change in VS Code
+    connection.console.log('We received a file change event');
+});
+
+// This handler provides the initial list of the completion items.
+connection.onCompletion(
+    (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+        // The pass parameter contains the position of the text document in
+        // which code complete got requested. For the example we ignore this
+        // info and always provide the same completion items.
+        return [
+            {
+                label: 'TypeScript',
+                kind: CompletionItemKind.Text,
+                data: 1
+            },
+            {
+                label: 'JavaScript',
+                kind: CompletionItemKind.Text,
+                data: 2
+            }
+        ];
+    }
+);
+
+// This handler resolves additional information for the item selected in
+// the completion list.
+connection.onCompletionResolve(
+    (item: CompletionItem): CompletionItem => {
+        if (item.data === 1) {
+            item.detail = 'TypeScript details';
+            item.documentation = 'TypeScript documentation';
+        } else if (item.data === 2) {
+            item.detail = 'JavaScript details';
+            item.documentation = 'JavaScript documentation';
+        }
+        return item;
+    }
+);
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
