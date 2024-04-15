acffa51fb42a92506b6be4831ebc8d9be067ea10
Gallus Huber <e51905700@student.tuwien.ac.at>
2024-04-15 13:47:36 +0200

    consider usage on rename

--- language-server/src/handler/ApplyLabelEditActionHandler.ts
+++ language-server/src/handler/ApplyLabelEditActionHandler.ts
@@ -8,6 +8,8 @@ import { Range } from 'vscode-languageserver-types';
 import { ER2CDS } from "../generated/ast.js";
 
 export class ApplyLabelEditActionHandler {
+    private workspaceEdit: WorkspaceEdit | undefined;
+
     public handle(action: ApplyLabelEditAction, server: ER2CDSDiagramServer, services: ER2CDSServices): Promise<void> {
         const sourceUriString = server.state.options?.sourceUri?.toString();
         if (!sourceUriString)
@@ -37,49 +39,65 @@ export class ApplyLabelEditActionHandler {
             // Entity-/Relationship-Label
             if (!type) {
                 model.entities.forEach((e) => {
-                    if (e.name === element && ((e.$cstNode) as CompositeCstNodeImpl).content[1].range)
-                        server.dispatch(this.createWorkspaceEditReplaceAction(server, sourceUri, ((e.$cstNode) as CompositeCstNodeImpl).content[1].range, action.text));
+                    if (e.name === element && ((e.$cstNode) as CompositeCstNodeImpl).content[1].range) {
+                        this.createWorkspaceEditReplaceAction(server, sourceUri, ((e.$cstNode) as CompositeCstNodeImpl).content[1].range, action.text);
+
+                        model.relationships.forEach((r) => {
+                            if (r.first?.target.$refText === element && r.first?.$cstNode?.range)
+                                this.createWorkspaceEditReplaceAction(server, sourceUri, r.first?.$cstNode?.range, action.text);
+
+                            if (r.second?.target.$refText === element && r.second?.$cstNode?.range)
+                                this.createWorkspaceEditReplaceAction(server, sourceUri, r.second?.$cstNode?.range, action.text);
+                        });
+                    }
                 });
 
                 model.relationships.forEach((r) => {
                     if (r.name === element && ((r.$cstNode) as CompositeCstNodeImpl).content[1].range)
-                        server.dispatch(this.createWorkspaceEditReplaceAction(server, sourceUri, ((r.$cstNode) as CompositeCstNodeImpl).content[1].range, action.text));
+                        this.createWorkspaceEditReplaceAction(server, sourceUri, ((r.$cstNode) as CompositeCstNodeImpl).content[1].range, action.text);
                 });
 
-            // Attribute-Label
+                // Attribute-Label
             } else {
                 model.entities.filter(e => e.name === element).forEach(e => e.attributes.forEach((a) => {
                     if (a.name === name) {
                         if (type === 'name' && ((a.$cstNode) as CompositeCstNodeImpl).content[0].range)
-                            server.dispatch(this.createWorkspaceEditReplaceAction(server, sourceUri, ((a.$cstNode) as CompositeCstNodeImpl).content[0].range, action.text));
+                            this.createWorkspaceEditReplaceAction(server, sourceUri, ((a.$cstNode) as CompositeCstNodeImpl).content[0].range, action.text);
 
                         if (type === 'datatype' && a.datatype?.$cstNode?.range)
-                            server.dispatch(this.createWorkspaceEditReplaceAction(server, sourceUri, a.datatype?.$cstNode?.range, action.text));
+                            this.createWorkspaceEditReplaceAction(server, sourceUri, a.datatype?.$cstNode?.range, action.text);
                     }
                 }));
             }
         }
 
+        if (this.workspaceEdit) {
+            const workspaceEditAction: WorkspaceEditAction = {
+                kind: WorkspaceEditAction.KIND,
+                workspaceEdit: this.workspaceEdit
+            }
+
+            server.dispatch(workspaceEditAction);
+        }
+
         return Promise.resolve();
     }
 
-    private createWorkspaceEditReplaceAction(server: ER2CDSDiagramServer, sourceUri: URI, range: Range, text: string): WorkspaceEditAction {
-        const workspaceEdit: WorkspaceEdit = {
-            changes: {
-                [sourceUri.toString()]: [
-                    {
-                        range: range,
-                        newText: text
-                    }
-                ]
+    private createWorkspaceEditReplaceAction(server: ER2CDSDiagramServer, sourceUri: URI, range: Range, text: string) {
+        if (!this.workspaceEdit) {
+            this.workspaceEdit = {
+                changes: {
+                    [sourceUri.toString()]: []
+                }
             }
         }
 
-        const workspaceEditAction: WorkspaceEditAction = {
-            kind: WorkspaceEditAction.KIND,
-            workspaceEdit: workspaceEdit
-        }
+        const textEdit = {
+            range: range,
+            newText: text
+        };
 
-        return workspaceEditAction;
+        if (this.workspaceEdit.changes)
+            this.workspaceEdit.changes[sourceUri.toString()].push(textEdit);
     }
 }
