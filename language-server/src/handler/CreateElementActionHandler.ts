199393a7fd6b299f78c6ee35aabcbcfc22e867dc
Gallus Huber <e51905700@student.tuwien.ac.at>
2024-05-12 12:53:58 +0200

    sort on creation

--- language-server/src/handler/CreateElementActionHandler.ts
+++ language-server/src/handler/CreateElementActionHandler.ts
@@ -1,14 +1,14 @@
 import { WorkspaceEditAction } from 'sprotty-vscode-protocol/lib/lsp/editing';
 import { WorkspaceEdit } from 'vscode-languageserver-protocol';
 import { Range, Position } from 'vscode-languageserver-types';
-import { TextDocument } from 'vscode-languageserver-textdocument';
 import { URI } from 'vscode-uri';
 import { CreateElementAction } from '../actions.js';
 import { ER2CDSServices } from '../er2cds-module.js';
 import { ER2CDSDiagramServer } from '../er2cds-diagram-server.js';
 import { SModelElement } from 'sprotty-protocol';
 import { NODE_ENTITY, NODE_RELATIONSHIP } from '../model.js';
-import { expandToString } from 'langium';
+import { LangiumDocument, expandToString } from 'langium';
+import { ER2CDS } from '../generated/ast.js';
 
 export class CreateElementActionHandler {
     public handle(action: CreateElementAction, server: ER2CDSDiagramServer, services: ER2CDSServices): Promise<void> {
@@ -26,23 +26,33 @@ export class CreateElementActionHandler {
 
         switch (action.elementType) {
             case NODE_ENTITY:
-                this.handleCreateEntity(server, sourceUri, document.textDocument);
+                this.handleCreateEntity(server, sourceUri, document);
                 break;
 
             case NODE_RELATIONSHIP:
-                this.handleCreateRelationship(server, sourceUri, document.textDocument);
+                this.handleCreateRelationship(server, sourceUri, document);
                 break;
         }
 
         return Promise.resolve();
     }
 
-    private handleCreateEntity(server: ER2CDSDiagramServer, sourceUri: URI, textDocument: TextDocument): void {
+    private handleCreateEntity(server: ER2CDSDiagramServer, sourceUri: URI, document: LangiumDocument): void {
+        const model = document.parseResult.value as ER2CDS;
+        const lastEntity = model.entities[model.entities.length - 1];
+
+        let range: Range;
+        if (lastEntity) {
+            range = Range.create(Position.create(lastEntity.$cstNode!.range.end.line, lastEntity.$cstNode!.range.end.character + 1), Position.create(lastEntity.$cstNode!.range.end.line, lastEntity.$cstNode!.range.end.character + 1));
+        } else {
+            range = Range.create(Position.create(document.textDocument.lineCount + 1, 0), Position.create(document.textDocument.lineCount + 1, 0));
+        }
+
         const workspaceEdit: WorkspaceEdit = {
             changes: {
                 [sourceUri.toString()]: [
                     {
-                        range: Range.create(Position.create(textDocument?.lineCount + 1, 0), Position.create(textDocument?.lineCount + 1, 0)),
+                        range: range,
                         newText: expandToString`
                         \n
                         entity ${this.getNewName(NODE_ENTITY, 'Entity', server.state.currentRoot.children)} {
@@ -60,12 +70,22 @@ export class CreateElementActionHandler {
         server.dispatch(workspaceEditAction);
     }
 
-    private handleCreateRelationship(server: ER2CDSDiagramServer, sourceUri: URI, textDocument: TextDocument): void {
+    private handleCreateRelationship(server: ER2CDSDiagramServer, sourceUri: URI, document: LangiumDocument): void {
+        const model = document.parseResult.value as ER2CDS;
+        const lastRelationship = model.relationships[model.relationships.length - 1];
+
+        let range: Range;
+        if (lastRelationship) {
+            range = Range.create(Position.create(lastRelationship.$cstNode!.range.end.line, lastRelationship.$cstNode!.range.end.character + 1), Position.create(lastRelationship.$cstNode!.range.end.line, lastRelationship.$cstNode!.range.end.character + 1));
+        } else {
+            range = Range.create(Position.create(document.textDocument.lineCount + 1, 0), Position.create(document.textDocument.lineCount + 1, 0));
+        }
+
         const workspaceEdit: WorkspaceEdit = {
             changes: {
                 [sourceUri.toString()]: [
                     {
-                        range: Range.create(Position.create(textDocument?.lineCount + 1, 0), Position.create(textDocument?.lineCount + 1, 0)),
+                        range: range,
                         newText: expandToString`
                         \n
                         relationship ${this.getNewName(NODE_RELATIONSHIP, 'Relationship', server.state.currentRoot.children)} {
